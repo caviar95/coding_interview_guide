42c3bdbb321ba7d0967bb6f61f8b1da96592065f
caviar95 <yzj1995@126.com>
2025-04-22 22:05:56 +0800

    get_min

---
+++ 1/get_min.hpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+#include <stack>
+
+using namespace std;
+
+// solution 1: less memory & more time
+class MyStack1 {
+    stack<int> data_;
+    stack<int> min_;
+
+public:
+    MyStack1() {}
+
+    void push(int newNum) {
+        if (min_.empty() || newNum <= min_.top()) {
+            min_.push(newNum);
+        }
+
+        data_.push(newNum);
+    }
+
+    int pop() {
+        if (data_.empty()) {
+            return -1; // 不使用异常
+        }
+
+        int value = data_.top();
+        if (value == min_.top()) {
+            min_.pop();
+        }
+        data_.pop();
+
+        return value;
+    }
+
+    int getMin() {
+        if (min_.empty()) {
+            return -1; // 不使用异常
+        }
+
+        return min_.top();
+    }
+};
+
+// solution 2: more memory & less time
+class MyStack2 {
+    stack<int> data_;
+    stack<int> min_;
+public:
+    void push(int newNum) {
+        if (min_.empty()) {
+            min_.push(newNum);
+        } else {
+            min_.push(min_.top() < newNum ? min_.top() : newNum);
+        }
+        data_.push(newNum);
+    }
+
+    int pop() {
+        if (data_.empty()) {
+            return -1; // 不使用异常
+        }
+
+        min_.pop();
+        int value = data_.top();
+        data_.pop();
+        return value;
+    }
+
+    int getMin() {
+        if (min_.empty()) {
+            return -1; // 不使用异常
+        }
+
+        return min_.top();
+    }
+};
+
---
+++ 1/test_get_min.cpp
@@ -0,0 +1,65 @@
+#include <gtest/gtest.h>
+#include "get_min.hpp" // 假设 get_min.cpp 在同一目录下
+
+// 测试 MyStack1 类
+TEST(MyStack1Test, PushAndPop) {
+    MyStack1 stack;
+    stack.push(5);
+    stack.push(3);
+    stack.push(7);
+    EXPECT_EQ(stack.pop(), 7);
+    EXPECT_EQ(stack.pop(), 3);
+    EXPECT_EQ(stack.pop(), 5);
+}
+
+TEST(MyStack1Test, GetMin) {
+    MyStack1 stack;
+    stack.push(5);
+    stack.push(3);
+    stack.push(7);
+    EXPECT_EQ(stack.getMin(), 3);
+    stack.pop();
+    EXPECT_EQ(stack.getMin(), 3);
+    stack.pop();
+    EXPECT_EQ(stack.getMin(), 5);
+}
+
+TEST(MyStack1Test, EmptyStack) {
+    MyStack1 stack;
+    EXPECT_EQ(stack.pop(), -1);
+    EXPECT_EQ(stack.getMin(), -1);
+}
+
+// 测试 MyStack2 类
+TEST(MyStack2Test, PushAndPop) {
+    MyStack2 stack;
+    stack.push(5);
+    stack.push(3);
+    stack.push(7);
+    EXPECT_EQ(stack.pop(), 7);
+    EXPECT_EQ(stack.pop(), 3);
+    EXPECT_EQ(stack.pop(), 5);
+}
+
+TEST(MyStack2Test, GetMin) {
+    MyStack2 stack;
+    stack.push(5);
+    stack.push(3);
+    stack.push(7);
+    EXPECT_EQ(stack.getMin(), 3);
+    stack.pop();
+    EXPECT_EQ(stack.getMin(), 3);
+    stack.pop();
+    EXPECT_EQ(stack.getMin(), 5);
+}
+
+TEST(MyStack2Test, EmptyStack) {
+    MyStack2 stack;
+    EXPECT_EQ(stack.pop(), -1);
+    EXPECT_EQ(stack.getMin(), -1);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
